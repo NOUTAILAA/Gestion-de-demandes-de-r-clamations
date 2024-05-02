cfc09b8b78bd6aee73334811876b08cf5cce8eca
NOUTAILAA <121615929+NOUTAILAA@users.noreply.github.com>
2024-05-02 19:56:53 +0100

    Delete PFAPROJECT directory

--- PFAPROJECT/src/main/java/com/sjprogramming/restapi/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sjprogramming.restapi.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sjprogramming.restapi.entity.User;
-public interface UserRepository extends JpaRepository<User , Integer> {
-
-}
